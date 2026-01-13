39033b5b88febd478c2556e6859ae89f6e7c7f46
adhvik2014 <jogimalik41@gmail.com>
2026-01-13 19:43:26 +0530

    Add files via upload

--- activity.py
+++ activity.py
@@ -1,7 +1,11 @@
-var = 10
-while var > 0:
-    var = var - 1
-    if var ==5:
-        continue
-    print('\nCurrent variable value :', var)
-    print ("\ngood bye!")
+for x in range(10):
+    if x % 20 == 0:
+        print("twist")
+    elif x % 15 == 0:
+        pass
+    elif x % 5 == 0:
+        print("fizz")
+    elif x % 3 == 0:
+        print("buzz")
+    else:
+        print(x)
